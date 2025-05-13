2345202dc6b20347115e849971ef8284841d9481
It-Me-Bk <toonixofficial@proton.me>
2025-05-13 16:50:58 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,8 +6,7 @@ import os
 BOT_TOKEN = "8081507527:AAHVRI2z4Ygx8ja9qLEz5RohPkqkC1jBma8"
 # From my.telegram.org/
 API_ID = "27623611"
-API_HASH =  "
-4a326a928083750585c7f96408ced199"
+API_HASH =  "4a326a928083750585c7f96408ced199"
 # For /log cmd
 OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "5180174682").split(" ")]
 # No time limit for this users
