0a5075d6dc40a3c8279df54ed586ae6bb9ab5304
It-Me-Bk <toonixofficial@proton.me>
2025-05-13 16:44:17 +0530

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,18 @@
+#KSBOTOFFICALS
+#DEVELOPER
+import os
+
+# Bot token from @botfather
+BOT_TOKEN = "8081507527:AAHVRI2z4Ygx8ja9qLEz5RohPkqkC1jBma8"
+# From my.telegram.org/
+API_ID = "27623611"
+API_HASH =  "
+4a326a928083750585c7f96408ced199"
+# For /log cmd
+OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "5180174682").split(" ")]
+# No time limit for this users
+AUTH_USERS = [5180174682]
+# Time gap after each request (in seconds) 
+TIME_GAP = 0
+BOT_USERNAME='Toonixrecoder_Bot' #bot username without@
+API_TOKEN='b7d9e1f7-12a6-4b8e-89a2-c3d124e5f6a7' #don't change this without developer permission
